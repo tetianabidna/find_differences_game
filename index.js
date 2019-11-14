a50621e57fb59b12a2d5d82d1a1ea548f7081579
Tetiana Bidna <dedivana78@gmail.com>
2019-11-14 20:36:55 +0100

    Load an img, define the mistaces area

---
+++ index.js
@@ -0,0 +1,24 @@
+var mistakes = [
+        { 'x': 145, 'y': 130 }, // Biene, Punkt, Flügel        
+        { 'x': 166, 'y': 213 }, // Biene, Linie, unter Bein        
+        { 'x': 277, 'y': 67 } // Stern      
+    ];
+
+$("#main_game_field").append(
+	"<img class='.play_img' src='assets/origin.png' usemap='#map' />" + 
+	"<map name='map'></map>"
+	);
+
+mistakes.forEach(element => $("map").append(
+	"<area class='mistake_area' shape='circle' "+
+	"coords='" + element['x'] + "," + element['y'] + "," + 15 + "' " +
+	"href='#' />" ));
+	
+
+$(".mistake_area").on("click", function(e){
+    e.preventDefault();
+
+	console.log("click");	
+});
+
+
