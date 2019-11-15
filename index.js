2971307bc51889c1b3a60f72d87969042c461df1
Tetiana Bidna <dedivana78@gmail.com>
2019-11-15 20:10:16 +0100

    img
    Now two images are displayed.
    Both have mistake areas.
    Coordinates of mistake areas are roughly adjusted

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-//All mistakes from the img
 var mistakes = [
         { 'x': 145, 'y': 130 }, // Biene, Punkt, Flügel        
         { 'x': 166, 'y': 213 }, // Biene, Linie, unter Bein        
         { 'x': 277, 'y': 67 } // Stern      
     ];
+	
+// Füge zwei Felder, wo Bilder gespeichert werden
+$("#main_game_field").append("<div class='div_with_img' id ='div_origin_img'></div>");
+$("#main_game_field").append("<div class='div_with_img' id ='div_clone_img'></div>");
 
-$("#main_game_field").append(
-	"<img class='.play_img' src='assets/origin.png' usemap='#map' />" + 
-	"<map name='map'></map>"
+// Füge zwei Bilder in die Felder hinzu, definiere Maps
+$("#div_origin_img").append(
+	"<img class='.play_img' id='img_orig' src='assets/origin.png' usemap='#map_origin' >" + 
+	"<map name='map_origin'></map>"
 	);
+$("#div_clone_img").append(
+	"<img class='play_img' src='assets/clone.png' usemap='#map_clone' />" + 
+	"<map name='map_clone'></map>"
+	);
+	
 
+// Koordinaten müssen skalierbar sein
 mistakes.forEach(element => $("map").append(
 	"<area class='mistake_area' shape='circle' "+
-	"coords='" + element['x'] + "," + element['y'] + "," + 15 + "' " +
+	"coords='" + (element['x'] * 400 / 558 )+ "," + (element['y'] * 500 / 656 )+ "," + 15 + "' " +
 	"href='#' />" ));
 	
 
@@ -21,5 +31,3 @@ $(".mistake_area").on("click", function(e){
 
 	console.log("click");	
 });
-
-
